23bf31ce483a1115f68ca501056ddd6581267e92
NguyenMauTuan123 <1812866@dlu.edu.vn>
2022-08-07 23:03:13 +0700

    post data body api send-image

--- src/services/sendImageDelivery/index.js
+++ src/services/sendImageDelivery/index.js
@@ -6,10 +6,17 @@ export async function sendImageDelivery({ id, image }) {
     id: id,
     image: image
   };
-
+  let formBody = [];
+  for (var property in reqParam) {
+    const encodedKey = encodeURIComponent(property);
+    const encodedValue = encodeURIComponent(reqParam[property]);
+    formBody.push(encodedKey + '=' + encodedValue);
+  }
+  formBody = formBody.join('&');
   try {
-    return await httpClient.post("send-image.html", reqParam);
+    return await httpClient.post("send-image.html", formBody);
   } catch (err) {
     return err;
-  }c 
+  }
+  
 }
